0d187547aadb6bf7bc2b9ac8ce611881fa271123
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2024-03-29 12:11:37 +0500

    refactor the folder structure

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        //Initial code
-        System.out.println("hello world!");
-    }
-}
---
+++ src/main/java/Introduction.java
@@ -0,0 +1,9 @@
+public class Introduction {
+    public Introduction() {
+
+    }
+
+    public void intro() {
+        System.out.println("Toys Warehouse");
+    }
+}
