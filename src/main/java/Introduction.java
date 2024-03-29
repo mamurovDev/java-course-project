584347cd2b91d511b02a9bba06657a7ebbeb6dc0
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2024-03-29 14:46:03 +0500

    configure maven

--- src/main/java/Introduction.java
+++ src/main/java/Introduction.java
@@ -1,12 +1,8 @@
 
 public class Introduction {
 
-    public Introduction() {
-
-    }
-
     public void showBrand() {
-        System.out.println("\u001B[1m" + "Toys warehouse");
+        System.out.println("\u001B[1m" + "\u001B[32m" + "Toys warehouse" + "\u001B[0m");
     }
 
     public void showCommands() {
