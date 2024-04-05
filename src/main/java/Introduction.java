81f2d4adee64e4e6d42afc08d878abd1da059836
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2024-04-05 13:46:57 +0500

    make Introduction's properties static

--- src/main/java/Introduction.java
+++ src/main/java/Introduction.java
@@ -1,27 +1,33 @@
 
 public class Introduction {
 
-    final private String[] commands = { "Exit", "Clear" };
-    final private String line = "------------------------------------------------------------------------";
-    final private String name = "Nurmuahmmad";
-    final private String email = "nurmuhammad_mamurjonov@student.itpu.uz";
+    final static private String[] commands = {
+            "list: Show all products",
+            "help: Show all available commands",
+            "clear: Clear all history in console",
+            "exit: exit the software"
+    };
+    final static private String line = "------------------------------------------------------------------------";
+    final static private String name = "Nurmuahmmad";
+    final static private String email = "nurmuhammad_mamurjonov@student.itpu.uz";
 
-    public void showBrand() {
+    public static void showBrand() {
         System.out.println(Colors.applyStyle(ColorsEnum.green, "\n Toys Warehouse", true));
         System.out.println(Colors.applyStyle(ColorsEnum.white, line, false));
         System.out
                 .println(Colors.applyStyle(ColorsEnum.green,
                         "By: " + name + " | " + email, false));
-        System.out.println(Colors.applyStyle(ColorsEnum.purple, "version: 1.0", false));
+        System.out.println(Colors.applyStyle(ColorsEnum.purple, "version: 1.0 | create date: 29.03.2024", false));
         System.out.println(Colors.applyStyle(ColorsEnum.white, line + "\n", false));
 
     }
 
-    public void showCommands() {
+    public static void showCommands() {
         System.out.println(Colors.applyStyle(ColorsEnum.white, "Available commands: ", true));
         for (String command : commands) {
             System.out.println("   * " + command);
         }
+        System.out.println(Colors.applyStyle(ColorsEnum.white, line + "\n", false));
     }
 
 }
